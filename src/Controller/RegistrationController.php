219bc668f2f0150f93a32820ebb5e04f7fd2579b
302869442 <j.vdv@outlook.com>
2022-03-30 12:34:17 +0200

    added more css

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -37,6 +37,7 @@ class RegistrationController extends AbstractController
 
             return $this->redirectToRoute('app_home');
         }
+        
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
